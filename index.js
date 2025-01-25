c172e0790d71173b604a9361a1fc82aa42bdf45f
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2025-01-25 12:28:02 +1030

    Add port 80 for production

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const config = require("./config.json");
+
+const PORT = config?.production ? 80 : 3000;
 
 app.get("*", (req, res) => {
     res.send("hi there");
