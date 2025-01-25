3cc8da41035af7b16e8d32e1e5cc51464221d6a3
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2025-01-25 12:25:48 +1030

    Remove unnecessary stuff for test

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.engine("html", require('ejs').renderFile);
-app.set("view-engine", "html");
-
 const PORT = 3000;
 
 app.get("*", (req, res) => {
